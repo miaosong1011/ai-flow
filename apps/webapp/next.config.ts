aff9561f6f36a7a095b00f5a1b334d566052c947
miaosong <605728539@qq.com>
2026-03-24 13:40:13 +0800

    fix: add serverExternalPackages config for Prisma

--- apps/webapp/next.config.ts
+++ apps/webapp/next.config.ts
@@ -7,7 +7,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    serverExternalPackages: ['@prisma/client', '@prisma/adapter-pg', 'pg'],
 }
 
 export default nextConfig
